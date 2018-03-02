925f2872f28e13ef3423c2b4ccc639c600293218
Matt Britt <matthewlbritt@gmail.com>
2018-03-02 15:06:10 -0600

    finished projects page

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -31,7 +31,10 @@ export default class ProjectsPage extends Component{
             <div>
                 <article>
                 <NavBar currentPage='projects'/>
-                    <h1>Projects Page</h1>
+                    <h1 id='projects-page-h1'>Projects Page</h1>
+                    <p id='projects-page-p'> The projects below were selected to show off the range of projects you can build using Arduino. 
+                         They range from scientific to novelty, from useful to entertaining.  Remember, there are many, many more projects out there to explore!
+                    </p>
                     <div className='grid-container'>
                         {boxes}
                     </div>
